e1f4f2b9f1fe1fae4ab21b55276d8d163c00fb0d
vilra830 <villafuerte.rhea@gmail.com>
2025-03-04 07:35:20 +1100

    adds local repo into remote

---
+++ src/main/java/io/nology/to_dos/ToDosApplication.java
@@ -0,0 +1,13 @@
+package io.nology.to_dos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/nology/to_dos/ToDosApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nology.to_dos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
